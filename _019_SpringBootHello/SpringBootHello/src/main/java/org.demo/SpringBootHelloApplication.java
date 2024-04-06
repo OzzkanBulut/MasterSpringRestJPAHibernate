e91db0b4b0ac8c98be9f8d41308c0e78c6234baf
OzzkanBulut <ozkanb66@gmail.com>
2024-04-06 20:00:43 +0300

    changes

--- _019_SpringBootHello/SpringBootHello/src/main/java/org.demo/SpringBootHelloApplication.java
+++ _019_SpringBootHello/SpringBootHello/src/main/java/org.demo/SpringBootHelloApplication.java
@@ -2,12 +2,21 @@ package org.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.websocket.reactive.WebSocketReactiveAutoConfiguration;
 
-@SpringBootApplication
+import javax.sql.DataSource;
+
+/*
+@SpringBootApplication is combination of these annotations:
+@EnableAutoConfiguration
+@SpringBootConfiguration
+@ComponentScan*/
+// Dont autoconfigure and create a bean for this class
+@SpringBootApplication(exclude = WebSocketReactiveAutoConfiguration.class)
 public class SpringBootHelloApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootHelloApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHelloApplication.class, args);
+    }
 
 }
