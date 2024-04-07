2550aeaf04b8144e2bbfa3ec22355e7a11cb3827
OzzkanBulut <ozkanb66@gmail.com>
2024-04-07 14:08:02 +0300

    changes

--- _019_SpringBootHello/SpringBootHello/src/main/java/org.demo/HelloController.java
+++ _019_SpringBootHello/SpringBootHello/src/main/java/org.demo/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
-    @RequestMapping("/home")
+    @RequestMapping(value = {"/home","","/"})
     public String helloPage(){
         return "helloo";
     }
